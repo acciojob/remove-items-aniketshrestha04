59e71c45cd1f8f9372234275ec6612634205d369
aniketshrestha04 <aniket.shrestha12@gmail.com>
2026-08-25 20:35:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const removeButton = document.querySelector('input[type="button"]');
+const dropdown = document.getElementById('colorSelect');
+
+removeButton.addEventListener('click', function(){
+	if (dropdown.selectedIndex !== -1) {
+		dropdown.remove(dropdown.selectedIndex);
+	}
+});
